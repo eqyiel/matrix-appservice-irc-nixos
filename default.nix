3f3b30e94fef2038b855aff2cafefe3bd7042b26
Ruben Maher <ruben@maher.fyi>
2018-06-17 21:02:33 +0930

    Make sure the same nodejs version that node2nix cooked with is used

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@
 with lib;
 
 let
-  package = pkgs.callPackage ./package.nix {};
+  package = pkgs.callPackage ./package.nix { nodejs = pkgs.nodejs-6_x; };
   cfg = config.services.matrix-appservice-irc;
   boolToStr = b: if b then "true" else "false";
   mkRoom = r: ''{
